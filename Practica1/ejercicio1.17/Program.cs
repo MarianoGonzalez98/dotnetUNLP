e5dc39d079507afba74849a8bdfa86101818e6c8
MarianoGonzalez98 <68166494+MarianoGonzalez98@users.noreply.github.com>
2022-03-10 13:51:11 -0300

    ejercicio 17 agregado

---
+++ Practica1/ejercicio1.17/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+
+17. Utilizar el operador ternario condicional para establecer el contenido de una variable entera con
+el menor valor de otras dos variables enteras.
+
+
+*/
+Console.Write("Ingrese la primer variable entera:");
+int var1= int.Parse(Console.ReadLine());
+Console.Write("Ingrese la segunda variable entera:");
+int var2= int.Parse(Console.ReadLine());
+int varMenor= (var1<var2) ? var1 : var2;
+Console.WriteLine($"La menor variable es : {varMenor}");
+
+
