6f8ad11bc62d77edbc112f2fbfac7cdf309ad4d7
MarianoGonzalez98 <68166494+MarianoGonzalez98@users.noreply.github.com>
2022-03-10 16:10:08 -0300

    exercise 1.18 added

---
+++ Practica1/ejercicio1.18/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+for (int i = 0; i <= 4; i++)
+{
+//string st = i < 3 ? i == 2 ? "dos" :  i == 1 ? "uno" : "< 1" : i < 4 ? "tres" : "> 3";
+
+string st = i < 3 ?   (  i == 2 ? "dos" :  ( i == 1 ? "uno" : "< 1" ) )  : (i < 4 ? "tres" : "> 3");
+Console.WriteLine(st);
+}
+
+
+
