b19b2c566559305e891ac022cd5f75fee22c20ee
MarianoGonzalez98 <68166494+MarianoGonzalez98@users.noreply.github.com>
2022-03-10 13:31:03 -0300

    ejercicios 2,3,4 agregados

---
+++ Practica1/ejercicio1.2/Program.cs
@@ -0,0 +1,6 @@
+﻿/*
+2. Investigar por las secuencias de escape \n, \t , \" y \\. 
+Escribir un programa que las utilice para imprimir distintos mensajes en la consola.
+*/
+Console.WriteLine("Salto de linea por \\n \n \tTab por \\t");
+Console.WriteLine("\\\" para comilla \" ");
---
+++ Practica1/ejercicio1.3/Program.cs
@@ -0,0 +1,10 @@
+﻿/*
+3. El carácter @ delante de un string desactiva los códigos de escape. Probar el siguiente
+fragmento de código, eliminar el carácter @ y utilizar las secuencias de escape necesarias para
+que el programa siga funcionando de igual manera
+*/
+
+Console.WriteLine("hellooo");
+string st = "c:\\windows\\system";
+Console.WriteLine(st);
+
