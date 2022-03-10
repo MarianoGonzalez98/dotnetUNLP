d4d27601f4276b70f5f5902e15a579349ccd4026
MarianoGonzalez98 <68166494+MarianoGonzalez98@users.noreply.github.com>
2022-03-10 13:46:00 -0300

    ejercicios 10, 11,12 agregados

---
+++ Practica1/ejercicio1.11/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+
+11. Comprobar el funcionamiento del siguiente fragmento de código, analizar el resultado y
+contestar las preguntas.
+
+¿Qué se puede concluir respecto del operador de división “/” ?
+-La division devuelve un numero entero si los 2 operandos son enteros. 
+    Si al menos uno es decimal, devuelve un numero decimal.
+
+
+¿Cómo funciona el operador + entre un string y un dato numérico?
+-Concatena el numero al string.
+*/
+
+
+Console.WriteLine("10/3 = " + 10 / 3);
+Console.WriteLine("10.0/3 = " + 10.0 / 3);
+Console.WriteLine("10/3.0 = " + 10 / 3.0);
+int a = 10, b = 3;
+Console.WriteLine("Si a y b son variables enteras, si a=10 y b=3");
+Console.WriteLine("entonces a/b = " + a / b);
+double c = 3;
+Console.WriteLine("Si c es una variable double, c=3");
+Console.WriteLine("entonces a/c = " + a / c);
---
+++ Practica1/ejercicio1.12/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+
+12. Escribir un programa que imprima todos los divisores de un número entero ingresado desde la
+consola. Para obtener el entero desde un string st utilizar int.Parse(st).
+
+
+*/
+Console.WriteLine("Ingrese un numero entero: ");
+int num = int.Parse(Console.ReadLine());
+Console.Write("Sus divisores son: ");
+
+for (int i = 1; i <= num; i++)
+{
+    if (num%i==0)
+    {
+        Console.Write(i+" ");
+    }
+}
