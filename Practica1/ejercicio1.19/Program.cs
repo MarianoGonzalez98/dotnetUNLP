7cb36f5941b603a38258d77bf166f920ffb8aca3
MarianoGonzalez98 <68166494+MarianoGonzalez98@users.noreply.github.com>
2022-03-10 23:47:55 -0300

    exercise 1.19 added

---
+++ Practica1/ejercicio1.19/Program.cs
@@ -0,0 +1,13 @@
+﻿//1. int a, b, c;
+//2. int a; int b; int c, d;
+//3. int a = 1; int b = 2; int c = 3;
+//4. int b; int c; int a = b = c = 1;
+//5. int c; int a, b = c = 1;
+//6. int c; int a = 2, b = c = 1;
+//7. int a = 2, b, c, d = 2 * a;
+//8. int a = 2, int b = 3, int c = 4; error
+//9. int a = 2; b = 3; c = 4; error
+//10. int a; int c = a; error
+//11. char c = 'A', string st = "Hola"; error
+//12. char c = 'A'; string st = "Hola"; error
+//13. char c = 'A', st = "Hola"; error
