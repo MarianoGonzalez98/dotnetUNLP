f5d51ec0a38f14b3919d357e3fe14afe0a310815
MarianoGonzalez98 <68166494+MarianoGonzalez98@users.noreply.github.com>
2022-03-17 21:07:22 -0300

    exercises 1,2,3 added

---
+++ practica2/ejercicio2_2/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//2. Sea el siguiente código:
+object o1 = "A";
+object o2 = o1;
+o2 = "Z";
+Console.WriteLine(o1 + " " + o2);
+/*
+El tipo object es un tipo referencia, por lo tanto luego de la sentencia o2 = o1 ambas
+variables están apuntando a la misma dirección. ¿Cómo explica entonces que el resultado en la consola no sea “Z Z”?
+
+porque en la linea < o2 = "Z"; > , la variable o2 va a tomar una nueva referencia y descartar la anterior
+
+??
+
+*/
+
+
+Console.WriteLine("Hello, World!");
---
+++ practica2/ejercicio2_3/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+/* 3. Analizar la siguiente porción de código para calcular la sumatoria de 1 a 10. ¿Cuál es el error?
+¿Qué hace realmente?
+
+Tiene un ; al final de la linea del while, queda en una iteracion permanente como si fuera while(1<=10){};
+
+*/
+
+int sum = 0;
+int i = 1;
+while (i <= 10);
+{
+    sum += i++;
+}
+
+Console.WriteLine(sum);
