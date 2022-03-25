7ed9fb1f36f162ef38453c40d0cddfd8052b8cbb
MarianoGonzalez98 <68166494+MarianoGonzalez98@users.noreply.github.com>
2022-03-25 14:59:23 -0300

    Create ejercicio3_15.cs

---
+++ practica3/ejercicio3_15.cs
@@ -0,0 +1,16 @@
+/* 15. ¿Qué salida por la consola produce el siguiente código? */
+int x = 0;
+try
+{
+    Console.WriteLine(1.0 / x);
+    Console.WriteLine(1 / x); //error aca: intento de division por 0
+    Console.WriteLine("todo OK");
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
+/* ¿Qué se puede inferir respecto de la excepción división por cero en relación al tipo de los operandos? 
+
+Esta permitido dividir un num flotante por 0
+*/
