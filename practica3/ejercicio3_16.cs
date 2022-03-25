5a9b1cb9bee303c734accfbdf539988c5c52f1ae
MarianoGonzalez98 <68166494+MarianoGonzalez98@users.noreply.github.com>
2022-03-25 15:00:22 -0300

    Create ejercicio3_16.cs

---
+++ practica3/ejercicio3_16.cs
@@ -0,0 +1,28 @@
+/* 
+16. Implementar un programa que permita al usuario ingresar números por la consola. Debe
+ingresarse un número por línea finalizado el proceso cuando el usuario ingresa una línea vacía.
+A medida que se van ingresando los valores el sistema debe mostrar por la consola la suma
+acumulada de los mismos. Utilizar la instrucción try/catch para validar que la entrada
+ingresada sea un número válido, en caso contrario advertir con un mensaje al usuario y
+proseguir con el ingreso de datos.
+*/
+
+string? input;
+double suma=0;
+Console.Write("Ingrese un numero, finalice con linea vacia: ");
+input= Console.ReadLine();
+
+while (input!="")
+{
+    try
+    {
+        suma+=double.Parse(input);
+        Console.WriteLine("Suma acumulada: "+suma);
+    }
+    catch
+    {
+        Console.WriteLine("Numero invalido");
+    }
+    Console.Write("Ingrese un numero, finalice con linea vacia: ");
+    input= Console.ReadLine();
+}
