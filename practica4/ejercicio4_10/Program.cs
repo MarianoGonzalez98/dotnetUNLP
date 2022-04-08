d96a7022b785e5d45ce8f9c4c71f908c523fe0c5
MarianoGonzalez98 <68166494+MarianoGonzalez98@users.noreply.github.com>
2022-04-08 15:51:00 -0300

    Add files via upload

---
+++ practica4/ejercicio4_10/Program.cs
@@ -0,0 +1,19 @@
+﻿// 10) ¿Qué se puede decir en relación a la sobrecarga de métodos en este ejemplo?
+
+class A
+{
+    public void Metodo(short n)
+    {
+        Console.Write("short {0} - ", n);
+    }
+    public void Metodo(int n)
+    {
+    Console.Write("int {0} - ", n);
+        }
+
+//es incorrecta porque este metodo tiene la misma cantidad y tipo de parametros que el anterior(un tipo de parametro integer)
+    public int Metodo(int n) 
+    {
+        return n + 10;
+    }
+}
---
+++ practica4/ejercicio4_9/Program.cs
@@ -0,0 +1,31 @@
+﻿/* 9) Prestar atención a los siguientes programas (ninguno funciona correctamente) */
+
+/* Foo f = new Foo();
+f.Imprimir();
+class Foo
+{
+    string? _bar;
+    public void Imprimir()
+    {
+        Console.WriteLine(_bar.Length);
+    }
+} */
+//se inicializa _bar en null automaticamente. Las variables de instancia se inicializan automaticamente
+
+/* Foo f = new Foo();
+f.Imprimir();
+class Foo
+{
+    public void Imprimir()
+    {
+        string? _bar;
+        Console.WriteLine(_bar.Length);
+    }
+} */
+//las variables locales no se inicializan automaticamente.
+
+/* ¿Qué se puede decir acerca de la inicialización de los objetos? ¿En qué casos son inicializados automáticamente y con qué valor? 
+
+
+
+*/
