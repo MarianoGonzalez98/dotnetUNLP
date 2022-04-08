84bf2bbabbd8b366d4f9c7ddd7386a4b870b447d
MarianoGonzalez98 <68166494+MarianoGonzalez98@users.noreply.github.com>
2022-04-08 16:03:10 -0300

    Add files via upload

---
+++ practica5/ejercicio5_10/Program.cs
@@ -0,0 +1,28 @@
+﻿/* 10) Identificar todos los miembros en la siguiente declaración de clase.
+
+Indicar si se trata de un constructor, método, campo, propiedad o indizador, si es estático o de instancia, y en caso que corresponda si es de sólo lectura, sólo escritura o lectura y escritura.
+
+En el caso de las propiedades indicar también si se trata de una propiedad auto-implementada.
+
+Nota: La clase compila perfectamente. Sólo prestar atención a la sintaxis, la semántica es irrelevante. 
+*/
+
+class A
+{
+    private static int a; //campo
+    private static readonly int b; //campo
+    A() { } //constructor
+    public A(int i) : this() { } //constructor
+    static A() => b = 2; //constructor?? (revisar)
+    int c; //campo
+    public static void A1() => a = 1; // metodo estatico
+    public int A1(int a) => A.a + a; //metodo de instancia
+    public static int A2; //campo estatico
+    static int A3 => 3; //propiedad estatica de solo lectura ??
+    private int A4() => 4; //metodo de instancia
+    public int A5 { get => 5; }
+    int A6 { set => c = value; }
+    public int A7 { get; set; }
+    public int A8 { get; } = 8;
+    public int this[int i] => i;
+}
---
+++ practica5/ejercicio5_9/Program.cs
@@ -0,0 +1,22 @@
+﻿//9) ¿Cuál es el error en el siguiente programa?
+//Retorna marca(el campo privado) cuando deberia retornar la propiedad Marca 
+
+Auto a = new Auto();
+a.Marca = "Ford";
+Console.WriteLine(a.Marca);
+
+class Auto
+{
+    private string marca;
+    public string Marca
+    {
+        set
+        {
+            Marca = value;
+        }
+        get
+        {
+            return marca;
+        }
+    }
+}
